4e4cc8795c2b14ba0bc485c39e0d54e284e8651c
KodeJaiSurya <59638236+KodeJaiSurya@users.noreply.github.com>
2025-10-30 15:41:10 -0400

    Revert "Fixing limits"
    This reverts commit 41acb8bd84ff209337119f63337c3b4a8b25857c.

--- lib/chatbot-api/functions/functions.ts
+++ lib/chatbot-api/functions/functions.ts
@@ -795,7 +795,7 @@ export class LambdaFunctionStack extends cdk.Stack {
           BUCKET: props.ffioNofosBucket.bucketName,
         },
         timeout: cdk.Duration.minutes(15),
-        reservedConcurrentExecutions: 5, // Process one NOFO at a time to avoid rate limits
+        reservedConcurrentExecutions: 100, // Limit to 100 concurrent workers
       }
     );
 
